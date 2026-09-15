ac8603ec64ab3d04d140c1b683e0cc289e8dfe57
KASHI220 <kashi220kp@gmail.com>
2026-09-15 23:49:31 +0530

    Add Question Service with MongoDB

---
+++ question/src/main/java/com/student/questions/entity/Question.java
@@ -0,0 +1,67 @@
+package com.student.questions.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "questions")
+public class Question {
+    @Id
+    private String id;
+    private String question;
+    private List<String> options;
+    private String correctAns;
+    private String category ;
+    private String difficulty;
+
+    public Question(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public String getCorrectAns() {
+        return correctAns;
+    }
+
+    public void setCorrectAns(String correctAns) {
+        this.correctAns = correctAns;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+}
---
+++ question/src/main/java/com/student/questions/repo/QuestionRepository.java
@@ -0,0 +1,20 @@
+package com.student.questions.repo;
+
+import com.student.questions.entity.Question;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends MongoRepository<Question, String> {
+
+    List<Question> findByCategory(String category);
+
+    @Aggregation(pipeline = {
+            "{ $match: { category: ?0 } }",
+            "{ $sample: { size: ?1 } }"
+    })
+    List<Question> findRandomQuestionsByCategory(String category, int count);
+}
